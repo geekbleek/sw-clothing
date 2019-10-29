6cd01c586136ea9e453cf007f1b8c24a681fdb55
jakereck@gmail.com <jakereck@gmail.com>
2019-10-28 19:50:33 -0700

    Integrated Firebase and sign in google functionality

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyD8hb1y29fdYhyWMzHZCiwrmgL9w2VJIHk",
+  authDomain: "sw-db-7894e.firebaseapp.com",
+  databaseURL: "https://sw-db-7894e.firebaseio.com",
+  projectId: "sw-db-7894e",
+  storageBucket: "sw-db-7894e.appspot.com",
+  messagingSenderId: "576667574388",
+  appId: "1:576667574388:web:384ef03ce8f09ccc86b121"
+}
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
